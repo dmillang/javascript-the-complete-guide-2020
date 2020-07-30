66f2eddddee5d7567bf113096db280ef829e3ab9
David Millan <hello@davidmillan.me>
2020-07-30 09:24:01 +0200

    Practice with template literals

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,7 +1,12 @@
-let currentResult = 0;
+const defaultResult = 0;
+let currentResult = defaultResult;
 
 currentResult = (currentResult + 10) * 3 / 2 - 1;
 
-let calculationDescription = '(' + currentResult + ' + 10) * 3 / 2 - 1'
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+
+let errorMessage = 'An error\noccurred!';
 
 outputResult(currentResult, calculationDescription);
+
+alert(errorMessage);
