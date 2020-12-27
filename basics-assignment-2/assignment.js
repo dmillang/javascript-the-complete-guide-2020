55ea32c64bfd918971bfd07b3b05f734c27dce46
David Millan <hello@davidmillan.me>
2020-12-27 01:09:14 +0000

    Complete basic assignment 2: functions

---
+++ basics-assignment-2/assignment.js
@@ -0,0 +1,26 @@
+const task3Element = document.getElementById('task-3');
+const functionType = 'WITH parameters';
+const st1 = 'First sentence + ';
+const st2 = 'Second sentence + ';
+const st3 = 'Third sentence';
+
+function noParamFunction() {
+    alert('This is the alert in a function WITHOUT parameters');
+}
+
+
+function paramFunction(name) {
+    alert('This is the alert in a function ' + functionType);
+}
+
+noParamFunction();
+paramFunction(functionType);
+task3Element.addEventListener('click', noParamFunction);
+
+function stringFunction(st1,st2,st3) {
+    const allStrings = st1 + st2 + st3;
+    alert(allStrings);
+}
+
+stringFunction('Hello, ','this are ','three strings');
+stringFunction(st1,st2,st3);
