d63bbf5f5879e7cbcc5f786a719455794ecc02c5
David Millan <hello@davidmillan.me>
2021-01-04 17:28:03 +0000

    understanding hoisting

--- behind-scenes-01/app.js
+++ behind-scenes-01/app.js
@@ -1,16 +1,20 @@
-var name = "David";
-let hobbies;
+// var name = "David";
+// let hobbies;
 
-if (name === 'David') {
-  hobbies = ['Sports','Cooking'];
-  console.log(hobbies);
-}
+// if (name === 'David') {
+//   hobbies = ['Sports','Cooking'];
+//   console.log(hobbies);
+// }
 
-function greet() {
-  var age = 31;
-  var name = 'Max'
-  console.log(name, age, hobbies);
-}
+// function greet() {
+//   var age = 31;
+//   var name = 'Max'
+//   console.log(name, age, hobbies);
+// }
 
-console.log(name, hobbies);
-greet();
+// console.log(name, hobbies);
+// greet();
+
+console.log(userName);
+
+let userName = 'David';
