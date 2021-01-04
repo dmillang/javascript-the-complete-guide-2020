2391f7b37c8094ea7e3b1a52aed25e0c7c93cd58
David Millan <hello@davidmillan.me>
2021-01-04 01:15:09 +0000

    small minor changes after checking solution

--- control-assignment-problem/assignment.js
+++ control-assignment-problem/assignment.js
@@ -2,7 +2,9 @@ const randomNumber = Math.random(); // produces random number between 0 (includi
 const secondRandomNumber = Math.random(); // produces random number between 0 (including) and 1 (excluding)
 
 if (randomNumber > 0.7) {
-  alert(`randomNumber is bigger than 0.7 [randomNumber: ${randomNumber.toFixed(2)}]`);
+  alert(
+    `randomNumber is bigger than 0.7 [randomNumber: ${randomNumber.toFixed(2)}]`
+  );
   console.log(`randomNumber = ${randomNumber.toFixed(2)}`);
 } else {
   console.log(`randomNumber = ${randomNumber.toFixed(2)}`);
@@ -13,7 +15,7 @@ if (randomNumber > 0.7) {
 const counter = 10;
 let numbersArray = [];
 
-for (i = 0; i < counter; i++) {
+for (let i = 0; i < counter; i++) {
   const randomNumber = Math.random(); // produces random number between 0 (including) and 1 (excluding)
   numbersArray.push(randomNumber.toFixed(2));
 }
@@ -24,34 +26,36 @@ console.log(numbersArray);
 
 console.log('--------------------');
 
-for (i = 0; i < numbersArray.length; i++) {
-  console.log(`${i+1}) ${numbersArray[i]}`)
+for (let i = 0; i < numbersArray.length; i++) {
+  console.log(`${i + 1}) ${numbersArray[i]}`);
 }
 
 console.log('--------------------');
 
 i = 0;
 for (const iterator of numbersArray) {
-  console.log(`${i+1} is ${iterator}`);
-  i++
+  console.log(`${i + 1} is ${iterator}`);
+  i++;
 }
 
 /**********************/
 
 console.log('--------------------');
 
-for (i = numbersArray.length -1; i >= 0; i--) {
+for (i = numbersArray.length - 1; i >= 0; i--) {
   console.log(`${i + 1}) ${numbersArray[i]}`);
 }
 
 /**********************/
+console.log(`randomNumber is ${randomNumber}`);
+console.log(`secondRandomNumber is ${secondRandomNumber}`);
 
-if ((randomNumber > 0.7 && secondRandomNumber > 0.7) || (randomNumber < 0.2 || secondRandomNumber < 0.2)) {
-  alert('a condition is met');
-  console.log(`randomNumber is ${randomNumber}`);
-  console.log(`secondRandomNumber is ${secondRandomNumber}`);
+if (
+  (randomNumber > 0.7 && secondRandomNumber > 0.7) ||
+  randomNumber < 0.2 ||
+  secondRandomNumber < 0.2
+) {
+  alert('Greater than 0.7 or not greater than 0.2');
 } else {
-  alert('no condition is met');
-  console.log(`randomNumber is ${randomNumber}`);
-  console.log(`secondRandomNumber is ${secondRandomNumber}`);
+  alert('No condition is met');
 }
