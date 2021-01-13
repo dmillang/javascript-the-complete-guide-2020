7f21b8212529c9faf44fa30692517eeebb4e52a9
David Millan <hello@davidmillan.me>
2021-01-13 01:20:44 +0000

    Complete Assignment 5 - DOM

---
+++ dom-assignment/assignment.js
@@ -0,0 +1,19 @@
+const li = document.querySelector('li#task-1');
+li.style.backgroundColor = 'black';
+
+const liID = document.getElementById('task-1');
+liID.style.color = 'white';
+
+//////////////////////
+
+const head = document.querySelector('head');
+const headTitle = head.querySelector('title');
+headTitle.innerText = 'Assignment - Super Solved!';
+
+const title = document.querySelector('head title');
+title.innerText = 'Assignment - Super Solved!';
+
+//////////////////////
+
+const h1 = document.querySelector('h1');
+h1.innerText = 'Assignment - Super Solved!';
