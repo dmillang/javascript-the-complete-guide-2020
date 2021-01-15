98b7a5902fa35453177d3a4d10d97295c9fdb128
David Millan <hello@davidmillan.me>
2021-01-15 13:16:37 +0000

    Styling DOM Elements

--- dom-01/app.js
+++ dom-01/app.js
@@ -8,8 +8,8 @@ h1.innerText = 'New title added via app.js'
 h1.style.color = 'white';
 h1.style.backgroundColor = '#000';
 
-const lastLi = document.querySelector('li:last-of-type');
-lastLi.style.color = 'red';
+// const lastLi = document.querySelector('li:last-of-type');
+// lastLi.style.color = 'red';
 
 const listItems = document.querySelectorAll('li');
 let i = 1;
@@ -18,3 +18,20 @@ for (const listItem of listItems) {
   listItem.innerText = 'Item ' + i;
   i += 1;
 }
+
+const section = document.querySelector('section');
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+  // if(section.className === 'red-bg visible') {
+  //   section.className = 'red-bg invisible';
+  // } else {
+  //   section.className = 'red-bg visible';
+  // }
+  // section.classList.toggle('visible');
+  section.classList.toggle('invisible');
+});
+
+// section.style.backgroundColor = 'blue';
+
+section.className = 'red-bg';
