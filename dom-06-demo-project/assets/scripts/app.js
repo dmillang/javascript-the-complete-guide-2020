62f240d6b15aaadbf24b6e8425b0595818151306
David Millan <hello@davidmillan.me>
2021-01-16 17:07:09 +0000

    movies - add backdrop functionality

--- dom-06-demo-project/assets/scripts/app.js
+++ dom-06-demo-project/assets/scripts/app.js
@@ -1,6 +1,25 @@
 const addMovieModal = document.getElementById('add-modal');
-const StartAddMovieButton = document.getElementById('add-movie');
+const startAddMovieButton = document.querySelector('header button');
+const backdrop = document.getElementById('backdrop');
+const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
 
-addMovieButton.addEventListener('click', () => {
-  modal.classList.toggle('visible');
-});
+const toggleBackdrop = () => {
+  backdrop.classList.toggle('visible');
+};
+
+const toggleMovieModal = () => { // function() {}
+  addMovieModal.classList.toggle('visible');
+  toggleBackdrop();
+};
+
+const cancelAddMovie = () => {
+  toggleMovieModal();
+};
+
+const backdropClickHandler = () => {
+  toggleMovieModal();
+};
+
+startAddMovieButton.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click', toggleMovieModal);
+cancelAddMovieButton.addEventListener('click', cancelAddMovie)
