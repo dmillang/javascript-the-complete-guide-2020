586fb05c479bfff03f3a40a2839768ffacaee7b0
David Millan <hello@davidmillan.me>
2021-01-16 16:30:31 +0000

    movies - select modal and menu cta button

---
+++ dom-06-demo-project/assets/scripts/app.js
@@ -0,0 +1,6 @@
+const addMovieModal = document.getElementById('add-modal');
+const StartAddMovieButton = document.getElementById('add-movie');
+
+addMovieButton.addEventListener('click', () => {
+  modal.classList.toggle('visible');
+});
