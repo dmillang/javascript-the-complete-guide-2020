d33957e1584af71e50460ce0d39597b7bb75e8b2
David Millan <hello@davidmillan.me>
2021-01-19 21:54:23 +0000

    creating arrays

---
+++ arrays-01/app.js
@@ -0,0 +1,13 @@
+const numbers = [1, 2, 3];
+console.log(numbers);
+
+// const moreNumbers = Array(5);
+// console.log(moreNumbers);
+
+// const yetMoreNumbers = Array.of(2, 3);
+// console.log(yetMoreNumbers);
+
+const listItems = document.querySelectorAll('li');
+
+const arrayListItems = Array.from(listItems);
+console.log(moreNumbers);
