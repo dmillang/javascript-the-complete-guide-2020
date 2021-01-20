5e813ffe31f72aa5bae1482c2e48df14266a6750
David Millan <hello@davidmillan.me>
2021-01-20 23:32:49 +0000

    Which Data Can You Store In Arrays?

--- arrays-01/app.js
+++ arrays-01/app.js
@@ -10,4 +10,17 @@ console.log(numbers);
 const listItems = document.querySelectorAll('li');
 
 const arrayListItems = Array.from(listItems);
-console.log(moreNumbers);
+console.log(arrayListItems);
+
+const hobbies = ['Surf','Cooking'];
+const personalData = ['John', 34, {moreDetails: []}];
+
+const analyticsData = [[1,6],[-5.4,3.2]];
+
+for (const dataPoint of analyticsData) {
+  for (const data of dataPoint) {
+    console.log(data);
+  }
+}
+
+console.log(personalData[0]);
