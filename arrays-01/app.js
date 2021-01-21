75315ecbf7f239c84af705966bb553389630f0b8
David Millan <hello@davidmillan.me>
2021-01-21 21:57:39 +0000

    Transforming Data with map()

--- arrays-01/app.js
+++ arrays-01/app.js
@@ -81,17 +81,28 @@
 
 // console.log(age32);
 
-const prices = [2.99, 1.99, 5.99, 9.99, 10.50];
-const tax = 0.24;
-const taxAdjustedPrices = [];
+// const prices = [2.99, 1.99, 5.99, 9.99, 10.50];
+// const tax = 0.24;
+// const taxAdjustedPrices = [];
 
 // for (const price of prices) {
 //   taxAdjustedPrices.push(price * (1 + tax));
 // }
 
-prices.forEach((price, idx, prices) => {
+// prices.forEach((price, idx, prices) => {
+//   const priceObj = {index: idx, taxAdjPrice: price * (1 + tax)};
+//   taxAdjustedPrices.push(priceObj.taxAdjPrice);
+// });
+
+// console.log(prices);
+// console.log(taxAdjustedPrices);
+
+const prices = [2.99, 1.99, 5.99, 9.99, 10.50];
+const tax = 0.24;
+
+const taxAdjustedPrices = prices.map((price, idx, prices) => {
   const priceObj = {index: idx, taxAdjPrice: price * (1 + tax)};
-  taxAdjustedPrices.push(priceObj.taxAdjPrice);
+  return priceObj.taxAdjPrice;
 });
 
 console.log(prices);
