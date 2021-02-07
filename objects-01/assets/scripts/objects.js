24368f6119ae2b4a56bf507be5839bfa03c76b24
David Millan <hello@davidmillan.me>
2021-02-07 21:24:28 +0000

    Adding, Modifying & Deleting Properties

--- objects-01/assets/scripts/objects.js
+++ objects-01/assets/scripts/objects.js
@@ -7,4 +7,18 @@ const person = {
   }
 }
 
-person.greet();
+// person.greet();
+
+// console.log(person.isAdmin); // returns undefined
+
+person.age = 31;
+
+person.age = null; // reset
+person.age = undefined; // dirty delete - do not use
+delete person.age; // delete properly
+
+
+person.isAdmin = true;
+
+console.log(person);
+
