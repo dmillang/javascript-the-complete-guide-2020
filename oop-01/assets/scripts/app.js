84369b0867293563b41f840ba2cbf389ad501d76
David Millan <hello@davidmillan.me>
2021-02-11 00:12:15 +0000

    Getting Started with OOP Code

--- oop-01/assets/scripts/app.js
+++ oop-01/assets/scripts/app.js
@@ -4,7 +4,7 @@ const productList = {
     {
       title: 'Soft pillow',
       imageUrl:
-        'https://hniesfp.imgix.net/8/images/detailed/145/taupe_mellow_cushion.jpg',
+        'https://i.etsystatic.com/19864675/r/il/533dfb/1940699137/il_570xN.1940699137_bnsg.jpg',
       price: 19.99,
       description: 'A soft taupe mellow pillow.',
     },
@@ -20,6 +20,24 @@ const productList = {
     const renderHook = document.getElementById('app');
     const prodList = document.createElement('ul');
     prodList.classList.add('product-list');
+    for (const product of this.products) {
+      const prodEl = document.createElement('li');
+      prodEl.className = 'product-item';
+      prodEl.innerHTML = `
+        <div>
+          <img src="${product.imageUrl}" alt="${product.title}">
+          <div class="product-item__content">
+            <h2>${product.title}</h2>
+            <h3>\$ ${product.price}</h3>
+            <p>${product.description}</p>
+            <button>Add to cart</button>
+          </div>
+        </div>
+      `;
+      prodList.append(prodEl);
+    }
     renderHook.append(prodList);
   }
 };
+
+productList.render();
