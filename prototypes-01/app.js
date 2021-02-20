b9dd9ef1143c1ac8edc091e6562ef971256d3243
David Millan <hello@davidmillan.me>
2021-02-20 09:09:28 +0000

    Classes & Prototypes

--- prototypes-01/app.js
+++ prototypes-01/app.js
@@ -1,51 +1,55 @@
 
-// class agedPerson {
-//   printAge() {
-//     console.log(this.age);
-//   }
-// }
-
-
-// class Person extends agedPerson {
-//   name = 'Max';
-
-//   constructor() {
-//     super();
-//     this.age = 30;
-//   }
-
-//   greet() {
-//     console.log(`Hi my name is ${this.name} and I'm ${this.age} years old.`);
-//   }
-// }
-
-function Person() {
-  this.name = 'Max';
-  this.age = 30;
-  this.greet = function() {
-    console.log(`Hi my name is ${this.name} and I'm ${this.age} years old.`);
-  };
-}
-
-Person.describe = function() {
-  console.log('Creating persons...');
-}
-
-Person.prototype.printAge = function() {
+class agedPerson {
+  printAge() {
     console.log(this.age);
+  }
 }
 
-const person = new Person();
 
-console.dir(Person);
+class Person extends agedPerson {
+  name = 'Max';
 
-person.greet();
-person.printAge();
-console.log(person.toString());
-console.log(person.__proto__);
+  constructor() {
+    super();
+    this.age = 30;
+  }
 
-const p2 = new person.__proto__.constructor();
-console.log(p2);
+  greet() {
+    console.log(`Hi my name is ${this.name} and I'm ${this.age} years old.`);
+  }
+}
 
-console.dir(Object.prototype);
-console.dir(Object.prototype.__proto__); // returns null  
+// function Person() {
+//   this.name = 'Max';
+//   this.age = 30;
+//   this.greet = function() {
+//     console.log(`Hi my name is ${this.name} and I'm ${this.age} years old.`);
+//   };
+// }
+
+// Person.describe = function() {
+//   console.log('Creating persons...');
+// }
+
+// Person.prototype.printAge = function() {
+//     console.log(this.age);
+// }
+
+// const person = new Person();
+
+// console.dir(Person);
+
+// person.greet();
+// person.printAge();
+// console.log(person.toString());
+// console.log(person.__proto__);
+
+// const p2 = new person.__proto__.constructor();
+// console.log(p2);
+
+// console.dir(Object.prototype);
+// console.dir(Object.prototype.__proto__); // returns null
+
+
+const p = new Person;
+console.log(p);
