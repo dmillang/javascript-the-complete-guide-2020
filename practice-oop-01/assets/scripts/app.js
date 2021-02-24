affe3d5e19da67a38265fca0f48aaaeb503a0b49
David Millan <hello@davidmillan.me>
2021-02-24 22:47:09 +0000

    Passing Method References Around

--- practice-oop-01/assets/scripts/app.js
+++ practice-oop-01/assets/scripts/app.js
@@ -1,20 +1,19 @@
 class ToolTip {}
 
 class ProjectItem {
-  constructor(id) {
+  constructor(id, updateProjectListsFunction) {
     this.id = id;
+    this.updateProjectListsHandler = updateProjectListsFunction;
     this.connectMoreInfoButton();
     this.connectSwitchButton();
   }
 
-  connectMoreInfoButton() {
-
-  }
+  connectMoreInfoButton() {}
 
   connectSwitchButton() {
     const projectItemElement = document.getElementById(this.id);
     const switchBtn = projectItemElement.querySelector('button:last-of-type');
-    switchBtn.addEventListener('click');
+    switchBtn.addEventListener('click', this.updateProjectListsHandler);
     console.log(switchBtn);
   }
 }
@@ -23,21 +22,27 @@ class ProjectList {
   projects = [];
 
   constructor(type) {
+    this.type = type;
     const prjItems = document.querySelectorAll(`#${type}-projects li`);
     for (const prjItem of prjItems) {
-      this.projects.push(new ProjectItem(prjItem.id));
+      this.projects.push(new ProjectItem(prjItem.id, this.switchProject.bind(this)));
     }
     console.log(this.projects);
   }
 
-  addProject() {
+  setSwitchHandlerFunction(switchHandlerFunction) {
+    this.switchHandler = switchHandlerFunction;
+  }
 
+  addProject() {
+    console.log(this);
   }
 
   switchProject(projectId) {
     // const projectIndex = this.projects.findIndex(p => p.id === projectId)
     // this.projects.splice(projectIndex, 1);
-    this.projects = this.projects.filter(p => p.id !== projectId);
+    this.switchHandler(this.projects.find((p) => projectId));
+    this.projects = this.projects.filter((p) => p.id !== projectId);
   }
 }
 
@@ -45,6 +50,12 @@ class App {
   static init() {
     const activeProjectsList = new ProjectList('active');
     const finishedProjecstList = new ProjectList('finished');
+    activeProjectsList.setSwitchHandlerFunction(
+      finishedProjecstList.addProject.bind(finishedProjecstList)
+    );
+    finishedProjecstList.setSwitchHandlerFunction(
+      activeProjectsList.addProject.bind(activeProjectsList)
+    );
   }
 }
 
