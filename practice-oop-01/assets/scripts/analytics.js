6213f4defa13b3712f67d3539c07a86bba5ae7a9
David Millan <hello@davidmillan.me>
2021-03-04 13:44:32 +0000

    Initial commit for Working with Events

--- practice-oop-01/assets/scripts/analytics.js
+++ practice-oop-01/assets/scripts/analytics.js
@@ -1,6 +1,6 @@
 const intervalId = setInterval(() => {
   console.log("Sending analytics data...");
-}, 3000)
+}, 5000)
 
 document.getElementById('stop-analytics-button').addEventListener('click', () => {
   clearInterval(intervalId);
