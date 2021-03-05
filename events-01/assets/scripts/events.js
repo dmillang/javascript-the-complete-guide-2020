d3255d35ce86df5f264d3e54484ab738e4d4a4bc
David Millan <hello@davidmillan.me>
2021-03-05 15:09:28 +0000

    Using Event Delegation

--- events-01/assets/scripts/events.js
+++ events-01/assets/scripts/events.js
@@ -40,21 +40,36 @@ const buttonClickHandler = (event) => {
 
 const form = document.querySelector('form');
 
-form.addEventListener('submit', event => {
-  event.preventDefault(   );
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
   console.log(event);
 });
 
 const div = document.querySelector('button').parentElement;
 
-div.addEventListener('click', event => {
+div.addEventListener('click', (event) => {
   console.log('clicked div');
   console.log(event);
 });
 
-button.addEventListener('click', event => {
+button.addEventListener('click', (event) => {
   event.stopPropagation();
   event.stopImmediatePropagation();
-  console.log('clicked button'); 
+  console.log('clicked button');
   console.log(event);
+});
+
+const listItems = document.querySelectorAll('li');
+const list = document.querySelector('ul');
+
+// listItems.forEach(listItem => {
+//   listItem.addEventListener('click', event => {
+//     event.target.classList.toggle('highlight');
+//   });
+// });
+
+list.addEventListener('click', (event) => {
+  // console.log(event.currentTarget);
+  // event.target.classList.toggle('highlight');
+  event.target.closest('li').classList.toggle('highlight');
 });
