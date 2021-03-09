3dde7a1c7e4d2f11165fc441baf1fc5ab8af7e2e
David Millan <hello@davidmillan.me>
2021-03-09 23:17:32 +0000

    The BigInt Type

--- nums-strings-01/app.js
+++ nums-strings-01/app.js
@@ -5,21 +5,32 @@
 
 // console.log(Math.pow(2,53)-1); // max number we can work with
 
-console.log((0.2 + 0.4) === 0.6 ? true : false); // false
+// console.log((0.2 + 0.4) === 0.6 ? true : false); // false
 
-console.log((5).toString(2)); // the 2 sets the string in the binary system
+// console.log((5).toString(2)); // the 2 sets the string in the binary system
 
-console.log((1/5).toString(2));
+// console.log((1/5).toString(2));
 
-console.log(0.33333 + 0.33333 + 0.33333); // 0.99999
-console.log(1/3 + 1/3 + 1/3); // 1
+// console.log(0.33333 + 0.33333 + 0.33333); // 0.99999
+// console.log(1/3 + 1/3 + 1/3); // 1
 
 
-console.log(0.2.toFixed(20)); // 0.20000000000000001110
+// console.log(0.2.toFixed(20)); // 0.20000000000000001110
 
-console.log((0.2 + 0.4).toFixed(1)); // 0.6
+// console.log((0.2 + 0.4).toFixed(1)); // 0.6
 
-let value = 20.2;
-console.log(value.toFixed(20)); // not 20.2 but lower: 20.19999999999999928946
+// let value = 20.2;
+// console.log(value.toFixed(20)); // not 20.2 but lower: 20.19999999999999928946
 
-console.log(value.toFixed(20) * 100); // reduce to integer
+// console.log(value.toFixed(20) * 100); // reduce to integer
+
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(900719925474099132219223112n);
+
+console.log(typeof Number.MAX_SAFE_INTEGER);
+console.log(typeof 900719925474099132219223112n);
+
+console.log(10n - 6n);
+console.log(parseInt(10n) - 6);
+console.log(10n - BigInt(6));
+
