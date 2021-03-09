a63cd41d10e56a4ad55db88f5b82e30e5246f1ae
David Millan <hello@davidmillan.me>
2021-03-09 23:11:06 +0000

    Floating Point (Im)Precision

--- nums-strings-01/app.js
+++ nums-strings-01/app.js
@@ -1,7 +1,25 @@
 
-console.log(Number.MAX_SAFE_INTEGER); // max integer number we can work with
-console.log(Number.MIN_SAFE_INTEGER); // min integer number we can work with
-console.log(Number.MAX_VALUE); // biggest value
+// console.log(Number.MAX_SAFE_INTEGER); // max integer number we can work with
+// console.log(Number.MIN_SAFE_INTEGER); // min integer number we can work with
+// console.log(Number.MAX_VALUE); // biggest value
 
-console.log(Math.pow(2,53)-1); // max number we can work with
+// console.log(Math.pow(2,53)-1); // max number we can work with
 
+console.log((0.2 + 0.4) === 0.6 ? true : false); // false
+
+console.log((5).toString(2)); // the 2 sets the string in the binary system
+
+console.log((1/5).toString(2));
+
+console.log(0.33333 + 0.33333 + 0.33333); // 0.99999
+console.log(1/3 + 1/3 + 1/3); // 1
+
+
+console.log(0.2.toFixed(20)); // 0.20000000000000001110
+
+console.log((0.2 + 0.4).toFixed(1)); // 0.6
+
+let value = 20.2;
+console.log(value.toFixed(20)); // not 20.2 but lower: 20.19999999999999928946
+
+console.log(value.toFixed(20) * 100); // reduce to integer
