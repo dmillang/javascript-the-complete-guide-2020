a4c8bbc99b51df18b2ff9263f799078a61143381
David Millan <hello@davidmillan.me>
2021-03-14 12:58:27 +0000

    Sending a DELETE Request

--- http-01/assets/scripts/app.js
+++ http-01/assets/scripts/app.js
@@ -2,6 +2,7 @@ const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
 const form = document.querySelector('#new-post form');
 const fetchButton = document.querySelector('#available-posts button');
+const postList = document.querySelector('ul');
 
 function sendHttpRequest(method, url, data) {
   const promise = new Promise((resolve, reject) => {
@@ -21,6 +22,7 @@ function sendHttpRequest(method, url, data) {
 }
 
 async function fetchPosts() {
+  listElement.innerHTML = '';
   const responseData = await sendHttpRequest(
     'GET',
     'https://jsonplaceholder.typicode.com/posts'
@@ -30,6 +32,7 @@ async function fetchPosts() {
     const postEl = document.importNode(postTemplate.content, true);
     postEl.querySelector('h2').textContent = post.title.toUpperCase();
     postEl.querySelector('p').textContent = post.body;
+    postEl.querySelector('li').id = post.id;
     listElement.append(postEl);
   }
 }
@@ -52,3 +55,10 @@ form.addEventListener('submit', event => {
   const enteredContent = event.currentTarget.querySelector('#content').value;
   createPost(enteredTitle, enteredContent);
 })
+
+postList.addEventListener('click', event => {
+  if (event.target.tagName === 'BUTTON') {
+    const postId = event.target.closest('li').id;
+    sendHttpRequest('DELETE', `https://jsonplaceholder.typicode.com/posts/${postId}`);
+  }
+});
